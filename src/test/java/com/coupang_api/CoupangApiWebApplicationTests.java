f6b4f8f5b46bb74b84a703cc05f83d1faab2243f
tw_soul <1013cm@naver.com>
2020-02-01 15:34:37 +0900

    프로젝트 시작

---
+++ src/test/java/com/coupang_api/CoupangApiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangApiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
