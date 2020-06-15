58908c2f356ad9ce73da8c658237ea1fe674c0bb
tw_soul <1013cm@naver.com>
2020-06-15 22:28:17 +0900

    access_key, secrete_key 쿠키 저장(진행중)

--- src/main/java/com/coupang_api/Web/WebController.java
+++ src/main/java/com/coupang_api/Web/WebController.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
@@ -25,18 +26,45 @@ public class WebController {
             @RequestParam("_search")String search_Str,
             @RequestParam("_secret_key")String SECRET_KEY,
             @RequestParam("_access_key")String ACCESS_KEY
-            ,Model model) throws IOException, ParseException {
+            ,Model model
+            , HttpServletResponse response
+            , HttpServletRequest request) throws IOException, ParseException {
 
+        //아무 문자도 입력하지 않았을때,
         if (search_Str.isEmpty()|| search_Str.equals("")){
 
         }else{
             String result = OpenApiTestApplication.deepLink_total(search_Str,ACCESS_KEY,SECRET_KEY);
             model.addAttribute("_result1",result);
+
+            Cookie cookie_ACCESS_KEY = new Cookie("ACCESS_KEY", ACCESS_KEY);
+            Cookie cookie_SECRET_KEY = new Cookie("SECRET_KEY", SECRET_KEY);
+
+            cookie_ACCESS_KEY.setMaxAge(0);
+            cookie_SECRET_KEY.setMaxAge(0);
+
+            cookie_ACCESS_KEY.setPath("/"); // 모든 경로에서 접근 가능 하도록 설정
+            cookie_SECRET_KEY.setPath("/"); // 모든 경로에서 접근 가능 하도록 설정
+
+            response.addCookie(cookie_ACCESS_KEY);
+            response.addCookie(cookie_SECRET_KEY);
+
         }
 
-//        model.addAttribute("_result","test");
-//        model.addAttribute("_result1",result);
+        Cookie[] Cookies = request.getCookies();
+        for(int i =0; i<Cookies.length;i++){
+            if(Cookies[i].getName().equals("ACCESS_KEY")){
+                model.addAttribute("_access_key",Cookies[i].getValue());
+
+            }else if(Cookies[i].getName().equals("SECRET_KEY")){
+                model.addAttribute("_secret_key",Cookies[i].getValue());
+            }
+        }
+
 
         return "main";
     }
+
+
+
 }
